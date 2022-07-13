12528b085239c9ad301bcb6078272188a1789054
RenrevZ <harvy275@gmail.com>
2022-07-13 11:37:21 +0800

    initialize Url routing

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -11,13 +11,38 @@
 
         public function __construct()
         {
-            $this->getUrl();
+            $url = $this->getURL();
+
+            //find the first value in the controller/ url
+            if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
+                // if the request url exist then set as controller
+                $this->currentController = ucwords($url[0]);
+
+                // uset the 0 index
+                unset($url[0]);
+
+            }
+
+            // Require the controller
+            require_once '../app/controllers/'. $this->currentController.'.php';
+
+            // instantiate controller class
+            $this->currentController = new $this->currentController;
         }
 
        public function getURL()
        {
            if(isset($_GET['url'])){
-               
+               // breaking the url using '/'
+               $url = rtrim($_GET['url'], '/');
+
+               //filter the url
+               $url = filter_var($url,FILTER_SANITIZE_URL);
+
+               // break the url into a array
+               $url = explode('/',$url);
+
+               return $url;
            }
        }
    }
