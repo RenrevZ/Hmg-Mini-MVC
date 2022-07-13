90de1c4cfb33ffa07357a2b8c10e44c5e74aacc1
RenrevZ <harvy275@gmail.com>
2022-07-13 16:22:40 +0800

    added database constant

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,9 @@
 <?php
+    // DB Params
+    define('DB_HOST','localhost');
+    define('DB_USER','root');
+    define('DB_PASS','');
+    define('DB_NAME','sharetech');
     // app root
     define('APPROOT',dirname(dirname(__FILE__)));
     // url root
--- public/js/main.js
+++
@@ -1,1 +0,0 @@
-alert('tanga tanaga ka');
