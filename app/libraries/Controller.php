b3714c8c1fc0aa975aa6c8b41941416dc845a50d
RenrevZ <harvy275@gmail.com>
2022-07-13 14:05:08 +0800

    added base controller

---
+++ app/libraries/Controller.php
@@ -0,0 +1,29 @@
+<?php
+    /*
+     * Based controller
+     * loads the models and views
+     */
+
+class Controller{
+    // load the model
+    public function model($model)
+    {
+        //require model file
+        require_once '../app/models'. $model.'.php';
+
+        // instantiate model
+        return new $model();
+    }
+
+    // load the view
+    public function view($view,$data = [])
+    {
+        // check for the view file if it exist
+        if(file_exists('../app/views/'.$view.'.php')){
+            require_once '../app/views/'.$view.'.php';
+        }else{
+            // means view doesnt exist
+            die('View does not exist');
+        }
+    }
+}
