2c3a33ae238b517f78109c86b58af954e524f076
RenrevZ <harvy275@gmail.com>
2022-07-13 16:48:16 +0800

    database test

---
+++ app/libraries/Database.php
@@ -0,0 +1,36 @@
+<?php
+    /*
+     * PDO database
+     * connect to the database
+     * crud functions and binding values
+     */
+
+class Database{
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $db_name = DB_NAME;
+    
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    public function __constructor()
+    {
+        //set dsn
+        $dsn = 'mysql:host='.$this->host.';dbname='.$this->db_name;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        // create PDO instance
+        try{
+
+        }catch (PDOException $e){
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+
+    }
+}
