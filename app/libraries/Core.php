9b16de40102fee334f732714c62d91f49b891825
RenrevZ <harvy275@gmail.com>
2022-07-13 11:19:22 +0800

    Initialize router

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,1 +1,24 @@
-hello
+<?php
+   /* 
+    - App core Class
+    - Creates a URL to load in Controller
+   */
+
+   class Core {
+        protected $currentController = 'Pages';
+        protected $currentMethod = 'index';
+        protected $params = [];
+
+        public function __construct()
+        {
+            $this->getUrl();
+        }
+
+       public function getURL()
+       {
+           if(isset($_GET['url'])){
+               
+           }
+       }
+   }
+
