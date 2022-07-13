11d7e51462ea56d69a8c58e462752c0f7ad970a1
RenrevZ <harvy275@gmail.com>
2022-07-13 14:09:42 +0800

    removed if

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,6 +1,6 @@
 <?php
 
-class Pages
+class Pages extends Controller
 {
     public function __construct()
     {
@@ -9,10 +9,10 @@ class Pages
 
     public function index()
     {
-        
+
     }
 
-    public function about($id){
-        echo $id;
+    public function about(){
+        
     }
 }
