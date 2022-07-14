b9be6127f68b4de9d95a4ecfdcd1122ea76eb28f
RenrevZ <harvy275@gmail.com>
2022-07-14 21:49:48 +0800

    change app name

--- app/config/config.php
+++ app/config/config.php
@@ -8,10 +8,10 @@
     define('DB_HOST','localhost');
     define('DB_USER','root');
     define('DB_PASS','');
-    define('DB_NAME','sharetech');
+    define('DB_NAME','Travel Share');
     // app root
     define('APPROOT',dirname(dirname(__FILE__)));
     // url root
-    define('URLROOT','http://localhost/HG-FRAME-MVC');
+    define('URLROOT','http://localhost/HG-FRAME-MVC');// insert your localhost url here
     // appname
     define('APPNAME','HG-FRAME-MVC');
